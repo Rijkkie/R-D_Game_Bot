24311c396531333fb64c3b63c47e92a0b37bc20e
Yoriyari <Yoriyari@users.noreply.github.com>
2021-04-19 11:41:06 +0200

    Update hangman.py
    This is more of a template for more cogs than an implementation of Hangman, right now.

--- cogs/hangman.py
+++ cogs/hangman.py
@@ -1,1 +1,44 @@
-//game codes go into this folder
+#===============================================================================
+# Hangman v0.1
+# - Last Updated: 18 Apr 2021
+#===============================================================================
+# Update History
+# ..............................................................................
+# 18 Apr 2021 - Started and finished file.
+#===============================================================================
+# Notes
+# ..............................................................................
+# - Code this. -YJ
+#===============================================================================
+# Description
+# ..............................................................................
+# hangman.py plays a game where player(s) take turns guessing letters in an
+# attempt to deduce a word. One user has to privately choose the word.
+# The bot can take this role by choosing a word from a dictionary.
+#===============================================================================
+
+#Import Modules
+import discord
+from discord.ext import commands
+
+#Cog Setup
+class Hangman(commands.Cog):
+    #Define variables.
+    def __init__(self, client):
+        self.client = client
+
+    #Report successful load.
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Hangman cog loaded.")
+
+    #Play hangman
+    @commands.command(aliases=["hm"])
+    async def hangman(self, ctx):
+        """
+        Code goes here
+        """
+
+#Client Setup
+def setup(client):
+    client.add_cog(Hangman(client))
