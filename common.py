6242177b5ad245afc58d480e7c0c82e1544c527c
Rijkkie <78078877+Rijkkie@users.noreply.github.com>
2021-04-25 15:22:27 +0200

    Update common.py
    Extra function for creating the winning message but again this can be omitted. Although the String to emoji could also be used later on.

--- common.py
+++ common.py
@@ -1,9 +1,10 @@
 #===============================================================================
 # Common v1.0
-# - Last Updated: 18 Apr 2021
+# - Last Updated: 24 Apr 2021
 #===============================================================================
 # Update History
 # ..............................................................................
+# 24 Apr 2021 - added two extra functions maybe?
 # 18 Apr 2021 - Started and finished file; Added generate_room_id, join_room,
 #               number_to_emoji, and emoji_to_number.
 #===============================================================================
@@ -98,3 +99,86 @@ def emoji_to_number(emoji):
         return 9
     if emoji == "🔟":
         return 10
+   
+def string_to_emoji(word):
+    emojis = ""
+    letters = word.upper()
+    for i in letters:
+        if ord(i) in range(65,91):
+            if i == "A":
+                emojis += ":regional_indicator_a:"
+            if i == "B":
+                emojis += ":regional_indicator_b:"
+            if i == "C":
+                emojis += ":regional_indicator_c:"
+            if i == "D":
+                emojis += ":regional_indicator_d:"
+            if i == "E":
+                emojis += ":regional_indicator_e:"
+            if i == "F":
+                emojis += ":regional_indicator_f:"
+            if i == "G":
+                emojis += ":regional_indicator_g:"
+            if i == "H":
+                emojis += ":regional_indicator_h:"
+            if i == "I":
+                emojis += ":regional_indicator_i:"
+            if i == "J":
+                emojis += ":regional_indicator_j:"
+            if i == "K":
+                emojis += ":regional_indicator_k:"
+            if i == "L":
+                emojis += ":regional_indicator_l:"
+            if i == "M":
+                emojis += ":regional_indicator_m:"
+            if i == "N":
+                emojis += ":regional_indicator_n:"
+            if i == "O":
+                emojis += ":regional_indicator_o:"
+            if i == "P":
+                emojis += ":regional_indicator_p:"
+            if i == "Q":
+                emojis += ":regional_indicator_q:"
+            if i == "R":
+                emojis += ":regional_indicator_r:"
+            if i == "S":
+                emojis += ":regional_indicator_s:"
+            if i == "T":
+                emojis += ":regional_indicator_t:"
+            if i == "U":
+                emojis += ":regional_indicator_u:"
+            if i == "V":
+                emojis += ":regional_indicator_v:"
+            if i == "W":
+                emojis += ":regional_indicator_w:"
+            if i == "X":
+                emojis += ":regional_indicator_x:"
+            if i == "Y":
+                emojis += ":regional_indicator_y:"
+            if i == "Z":
+                emojis += ":regional_indicator_z:"
+        elif ord(i) in range(48,58):
+            emojis += number_to_emoji(int(i))
+        else:
+            emojis += ":asterisk:"
+    return emojis
+
+def create_winner_message(winner):
+    winner_message = ":clap::regional_indicator_h::regional_indicator_a::regional_indicator_s::regional_indicator_w::regional_indicator_o::regional_indicator_n::regional_indicator_t::regional_indicator_h::regional_indicator_e::regional_indicator_g::regional_indicator_a::regional_indicator_m::regional_indicator_e::clap:"
+    audience = ":clap::clap::clap::clap::clap::clap::clap::clap::clap::clap::clap::clap::clap::clap::clap:"
+    message_length = 15
+    clap = ":clap:"
+    if len(winner)>message_length:
+            return audience + "\n" + winner_message + "\n" + audience
+    else:
+        start_position = 7
+        start_position = start_position - int(len(winner)/2)
+        end = 15 - start_position - len(winner)
+        winner_name = ""
+        for i in range(start_position):
+            winner_name += clap
+        winner_name += string_to_emoji(winner)
+        for i in range(end):
+            winner_name += clap
+        return audience +"\n" +  winner_name + "\n"  + winner_message + "\n" + audience
+
