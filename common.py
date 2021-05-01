acf522b1f220e7f72f09f1af7ef04d92d7a133fc
Yoriyari <Yoriyari@users.noreply.github.com>
2021-05-01 03:52:32 +0200

    Update common.py to v1.2
    Sessions are saved as Classes now, so the room-related functions needed some small changes to support this. Basically just changing session[0] to session.room_id. Improves readability. Going to need to update fourOnARow.py too (and maybe rename it to Connect Four instead).
    Figured it'd be better to have emoji_to_number and number_to_emoji return something at least, so if no numbers or emoji are found it'll return None.
    Added quit_room to remove players from sessions.
    Added card_deck_52 to request the standard 52-card deck.
    Added card_to_emoji which will return a custom emoji for any of the standard 52 cards (which I'm a little proud of, considering how many emoji I needed to upload to Discord).

--- common.py
+++ common.py
@@ -1,16 +1,18 @@
 #===============================================================================
-# Common v1.0
-# - Last Updated: 24 Apr 2021
+# Common v1.2
+# - Last Updated: 30 Apr 2021
 #===============================================================================
 # Update History
 # ..............................................................................
-# 24 Apr 2021 - added two extra functions maybe?
+# 30 Apr 2021 - Adjustments because sessions are Classes now; Now emoji/number
+#               returns None; Added quit_room, card_deck_52, card_to_emoji. -YJ
+# 24 Apr 2021 - added two extra functions maybe? -RK
 # 18 Apr 2021 - Started and finished file; Added generate_room_id, join_room,
-#               number_to_emoji, and emoji_to_number.
+#               number_to_emoji, and emoji_to_number. -YJ
 #===============================================================================
 # Notes
 # ..............................................................................
-# - Add more common features, like card deck stuff. -YJ
+# - Split this file into several files that can be imported separately. -YJ
 #===============================================================================
 # Description
 # ..............................................................................
@@ -30,27 +32,148 @@ def generate_room_id(game_sessions):
         for c in range(4):
             room_id += random.choice(alphabet)
         for session in game_sessions:
-            if session[0] == room_id:
+            if session.room_id == room_id:
                 room_id = ""
                 break
     return room_id
 
-def join_room(game_sessions, room_id, player_id, max_players):
+def join_room(game_sessions, room_id, player, max_players):
     #Check for existence of room ID
     for session in game_sessions:
-        if session[0] == room_id:
-            for player in session[2]:
-                if player == player_id:
-                    #Player is already in the room.
-                    return 1
-            if max_players <= 0 and len(session[2]) >= max_players:
-                #Room is full.
-                return 2
-            #Player is not in the room; Add player to room.
-            session[2].append(player_id)
-            return 0
-    #Room ID not found in any sessions.
-    return 3
+        if session.room_id == room_id:
+            for player_in_session in session.players:
+                if player_in_session == player:
+                    return 1 #Player is already in the room.
+            if max_players > 0 and len(session.players) >= max_players:
+                return 2 #Room is full.
+            session.add_player(player)
+            return 0 #Player was not in the room; Added player to room.
+    return 3 #Room ID not found in any sessions.
+
+def quit_room(game_sessions, room_id, player):
+    #Check for existence of room ID
+    for session in game_sessions:
+        if session.room_id == room_id:
+            for player_in_session in session.players:
+                if player_in_session.id == player.id:
+                    session.remove_player(player)
+                    return 0 #Player was in the room; Removed player from room.
+            return 1 #Player was not in the room.
+    return 2 #Room ID not found in any sessions.
+
+def card_deck_52():
+    #Would it be less efficient to have a for-loop generate this? It'd look better, but...
+    return ["1 h","2 h","3 h","4 h","5 h","6 h","7 h","8 h","9 h","10 h","11 h","12 h","13 h",
+            "1 d","2 d","3 d","4 d","5 d","6 d","7 d","8 d","9 d","10 d","11 d","12 d","13 d",
+            "1 s","2 s","3 s","4 s","5 s","6 s","7 s","8 s","9 s","10 s","11 s","12 s","13 s",
+            "1 c","2 c","3 c","4 c","5 c","6 c","7 c","8 c","9 c","10 c","11 c","12 c","13 c"]
+
+def card_to_emoji(card):
+    if card == "1 h": #Hearts
+        return "<:HA:716858601041428502>"
+    if card == "2 h":
+        return "<:H2:716858631936671804>"
+    if card == "3 h":
+        return "<:H3:716858652752871485>"
+    if card == "4 h":
+        return "<:H4:716858669446332426>"
+    if card == "5 h":
+        return "<:H5:716858683857829890>"
+    if card == "6 h":
+        return "<:H6:716858697732718684>"
+    if card == "7 h":
+        return "<:H7:716858732557893632>"
+    if card == "8 h":
+        return "<:H8:716858746474725386>"
+    if card == "9 h":
+        return "<:H9:716858760450146384>"
+    if card == "10 h":
+        return "<:H10:716858775570481223>"
+    if card == "11 h":
+        return "<:HJ:716858817094090832>"
+    if card == "12 h":
+        return "<:HQ:716858849868513341>"
+    if card == "13 h":
+        return "<:HK:716858861247791196>"
+    if card == "1 d": #Diamonds
+        return "<:DA:716858907821342740>"
+    if card == "2 d":
+        return "<:D2:716858925395476510>"
+    if card == "3 d":
+        return "<:D3:716858934874341446>"
+    if card == "4 d":
+        return "<:D4:716858950133350444>"
+    if card == "5 d":
+        return "<:D5:716858961730732043>"
+    if card == "6 d":
+        return "<:D6:716858984522580008>"
+    if card == "7 d":
+        return "<:D7:716858996459307088>"
+    if card == "8 d":
+        return "<:D8:716859011491954720>"
+    if card == "9 d":
+        return "<:D9:716859023093399643>"
+    if card == "10 d":
+        return "<:D10:716859063111254018>"
+    if card == "11 d":
+        return "<:DJ:716859091942899784>"
+    if card == "12 d":
+        return "<:DQ:716859115149852722>"
+    if card == "13 d":
+        return "<:DK:716859126088728607>"
+    if card == "1 s": #Spades
+        return "<:SA:716859337850748979>"
+    if card == "2 s":
+        return "<:S2:716859353390383176>"
+    if card == "3 s":
+        return "<:S3:716859364945952788>"
+    if card == "4 s":
+        return "<:S4:716859377793105980>"
+    if card == "5 s":
+        return "<:S5:716859390275354624>"
+    if card == "6 s":
+        return "<:S6:716859401415426080>"
+    if card == "7 s":
+        return "<:S7:716859412697841776>"
+    if card == "8 s":
+        return "<:S8:716859444427882556>"
+    if card == "9 s":
+        return "<:S9:716859456889028679>"
+    if card == "10 s":
+        return "<:S10:716859469392379975>"
+    if card == "11 s":
+        return "<:SJ:716859500518440961>"
+    if card == "12 s":
+        return "<:SQ:716859517417029652>"
+    if card == "13 s":
+        return "<:SK:716859531400839178>"
+    if card == "1 c": #Clubs
+        return "<:CA:716859547956019250>"
+    if card == "2 c":
+        return "<:C2:716859561998418021>"
+    if card == "3 c":
+        return "<:C3:716859569946492959>"
+    if card == "4 c":
+        return "<:C4:716859578154876929>"
+    if card == "5 c":
+        return "<:C5:716859589160730674>"
+    if card == "6 c":
+        return "<:C6:716859596479922297>"
+    if card == "7 c":
+        return "<:C7:716859608131698751>"
+    if card == "8 c":
+        return "<:C8:716859613676437514>"
+    if card == "9 c":
+        return "<:C9:716859631120547922>"
+    if card == "10 c":
+        return "<:C10:716859635826556980>"
+    if card == "11 c":
+        return "<:CJ:716859641388335124>"
+    if card == "12 c":
+        return "<:CQ:716859653182718083>"
+    if card == "13 c":
+        return "<:CK:716859657972350977>"
+    return None
 
 def number_to_emoji(number):
     if number == 0:
@@ -75,6 +198,7 @@ def number_to_emoji(number):
         return "9️⃣"
     if number == 10:
         return "🔟"
+    return None
 
 def emoji_to_number(emoji):
     if emoji == "0️⃣":
@@ -99,6 +223,7 @@ def emoji_to_number(emoji):
         return 9
     if emoji == "🔟":
         return 10
+    return None
    
 def string_to_emoji(word):
     emojis = ""
@@ -181,4 +306,3 @@ def create_winner_message(winner):
         for i in range(end):
             winner_name += clap
         return audience +"\n" +  winner_name + "\n"  + winner_message + "\n" + audience
-
