c9f3f59e64bd69250d0d0cf310732ab15e96f059
Yoriyari <Yoriyari@users.noreply.github.com>
2021-05-19 19:58:19 +0200

    common.py v1.3
    Added Card class and changed card_to_emoji to a dict. Also, made card_deck_52 better.

--- common.py
+++ common.py
@@ -1,9 +1,11 @@
 #===============================================================================
-# Common v1.2
-# - Last Updated: 30 Apr 2021
+# Common v1.3
+# - Last Updated: 17 May 2021
 #===============================================================================
 # Update History
 # ..............................................................................
+# 17 May 2021 - Added Card class and changed card_to_emoji to a dict. Also,
+#               made card_deck_52 better. -YJ
 # 30 Apr 2021 - Adjustments because sessions are Classes now; Now emoji/number
 #               returns None; Added quit_room, card_deck_52, card_to_emoji. -YJ
 # 24 Apr 2021 - added two extra functions maybe? -RK
@@ -13,6 +15,9 @@
 # Notes
 # ..............................................................................
 # - Split this file into several files that can be imported separately. -YJ
+# - Add an expiry timer on that room ID or something, to prevent users from just
+#   starting a bunch of empty rooms and hogging all the IDs. -YJ
+# - A lot more room stuff could be made into common code. -YJ
 #===============================================================================
 # Description
 # ..............................................................................
@@ -23,6 +28,92 @@
 #Import Modules
 import random
 
+#Card Dict
+card_emoji = {
+    "Hearts": {
+        1: "<:HA:716858601041428502>",
+        2: "<:H2:716858631936671804>",
+        3: "<:H3:716858652752871485>",
+        4: "<:H4:716858669446332426>",
+        5: "<:H5:716858683857829890>",
+        6: "<:H6:716858697732718684>",
+        7: "<:H7:716858732557893632>",
+        8: "<:H8:716858746474725386>",
+        9: "<:H9:716858760450146384>",
+        10: "<:H10:716858775570481223>",
+        11: "<:HJ:716858817094090832>",
+        12: "<:HQ:716858849868513341>",
+        13: "<:HK:716858861247791196>"
+    },
+    "Diamonds": {
+        1: "<:DA:716858907821342740>",
+        2: "<:D2:716858925395476510>",
+        3: "<:D3:716858934874341446>",
+        4: "<:D4:716858950133350444>",
+        5: "<:D5:716858961730732043>",
+        6: "<:D6:716858984522580008>",
+        7: "<:D7:716858996459307088>",
+        8: "<:D8:716859011491954720>",
+        9: "<:D9:716859023093399643>",
+        10: "<:D10:716859063111254018>",
+        11: "<:DJ:716859091942899784>",
+        12: "<:DQ:716859115149852722>",
+        13: "<:DK:716859126088728607>"
+    },
+    "Spades": {
+        1: "<:SA:716859337850748979>",
+        2: "<:S2:716859353390383176>",
+        3: "<:S3:716859364945952788>",
+        4: "<:S4:716859377793105980>",
+        5: "<:S5:716859390275354624>",
+        6: "<:S6:716859401415426080>",
+        7: "<:S7:716859412697841776>",
+        8: "<:S8:716859444427882556>",
+        9: "<:S9:716859456889028679>",
+        10: "<:S10:716859469392379975>",
+        11: "<:SJ:716859500518440961>",
+        12: "<:SQ:716859517417029652>",
+        13: "<:SK:716859531400839178>"
+    },
+    "Clubs": {
+        1: "<:CA:716859547956019250>",
+        2: "<:C2:716859561998418021>",
+        3: "<:C3:716859569946492959>",
+        4: "<:C4:716859578154876929>",
+        5: "<:C5:716859589160730674>",
+        6: "<:C6:716859596479922297>",
+        7: "<:C7:716859608131698751>",
+        8: "<:C8:716859613676437514>",
+        9: "<:C9:716859631120547922>",
+        10: "<:C10:716859635826556980>",
+        11: "<:CJ:716859641388335124>",
+        12: "<:CQ:716859653182718083>",
+        13: "<:CK:716859657972350977>"
+    }
+}
+
+#Card Setup
+class Card:
+    #Define variables
+    def __init__(self, suit: str, value: int):
+        self.__suit = suit
+        self.__value = value
+        self.__emoji = card_emoji[suit][value]
+
+    @property
+    def suit(self):
+        return self.__suit
+
+    @property
+    def value(self):
+        return self.__value
+
+    @property
+    def emoji(self):
+        return self.__emoji
+
+
+#Common Functions
 def generate_room_id(game_sessions):
     #Generate a new unique ID for game room
     room_id = ""
@@ -62,118 +153,12 @@ def quit_room(game_sessions, room_id, player):
     return 2 #Room ID not found in any sessions.
 
 def card_deck_52():
-    #Would it be less efficient to have a for-loop generate this? It'd look better, but...
-    return ["1 h","2 h","3 h","4 h","5 h","6 h","7 h","8 h","9 h","10 h","11 h","12 h","13 h",
-            "1 d","2 d","3 d","4 d","5 d","6 d","7 d","8 d","9 d","10 d","11 d","12 d","13 d",
-            "1 s","2 s","3 s","4 s","5 s","6 s","7 s","8 s","9 s","10 s","11 s","12 s","13 s",
-            "1 c","2 c","3 c","4 c","5 c","6 c","7 c","8 c","9 c","10 c","11 c","12 c","13 c"]
-
-def card_to_emoji(card):
-    if card == "1 h": #Hearts
-        return "<:HA:716858601041428502>"
-    if card == "2 h":
-        return "<:H2:716858631936671804>"
-    if card == "3 h":
-        return "<:H3:716858652752871485>"
-    if card == "4 h":
-        return "<:H4:716858669446332426>"
-    if card == "5 h":
-        return "<:H5:716858683857829890>"
-    if card == "6 h":
-        return "<:H6:716858697732718684>"
-    if card == "7 h":
-        return "<:H7:716858732557893632>"
-    if card == "8 h":
-        return "<:H8:716858746474725386>"
-    if card == "9 h":
-        return "<:H9:716858760450146384>"
-    if card == "10 h":
-        return "<:H10:716858775570481223>"
-    if card == "11 h":
-        return "<:HJ:716858817094090832>"
-    if card == "12 h":
-        return "<:HQ:716858849868513341>"
-    if card == "13 h":
-        return "<:HK:716858861247791196>"
-    if card == "1 d": #Diamonds
-        return "<:DA:716858907821342740>"
-    if card == "2 d":
-        return "<:D2:716858925395476510>"
-    if card == "3 d":
-        return "<:D3:716858934874341446>"
-    if card == "4 d":
-        return "<:D4:716858950133350444>"
-    if card == "5 d":
-        return "<:D5:716858961730732043>"
-    if card == "6 d":
-        return "<:D6:716858984522580008>"
-    if card == "7 d":
-        return "<:D7:716858996459307088>"
-    if card == "8 d":
-        return "<:D8:716859011491954720>"
-    if card == "9 d":
-        return "<:D9:716859023093399643>"
-    if card == "10 d":
-        return "<:D10:716859063111254018>"
-    if card == "11 d":
-        return "<:DJ:716859091942899784>"
-    if card == "12 d":
-        return "<:DQ:716859115149852722>"
-    if card == "13 d":
-        return "<:DK:716859126088728607>"
-    if card == "1 s": #Spades
-        return "<:SA:716859337850748979>"
-    if card == "2 s":
-        return "<:S2:716859353390383176>"
-    if card == "3 s":
-        return "<:S3:716859364945952788>"
-    if card == "4 s":
-        return "<:S4:716859377793105980>"
-    if card == "5 s":
-        return "<:S5:716859390275354624>"
-    if card == "6 s":
-        return "<:S6:716859401415426080>"
-    if card == "7 s":
-        return "<:S7:716859412697841776>"
-    if card == "8 s":
-        return "<:S8:716859444427882556>"
-    if card == "9 s":
-        return "<:S9:716859456889028679>"
-    if card == "10 s":
-        return "<:S10:716859469392379975>"
-    if card == "11 s":
-        return "<:SJ:716859500518440961>"
-    if card == "12 s":
-        return "<:SQ:716859517417029652>"
-    if card == "13 s":
-        return "<:SK:716859531400839178>"
-    if card == "1 c": #Clubs
-        return "<:CA:716859547956019250>"
-    if card == "2 c":
-        return "<:C2:716859561998418021>"
-    if card == "3 c":
-        return "<:C3:716859569946492959>"
-    if card == "4 c":
-        return "<:C4:716859578154876929>"
-    if card == "5 c":
-        return "<:C5:716859589160730674>"
-    if card == "6 c":
-        return "<:C6:716859596479922297>"
-    if card == "7 c":
-        return "<:C7:716859608131698751>"
-    if card == "8 c":
-        return "<:C8:716859613676437514>"
-    if card == "9 c":
-        return "<:C9:716859631120547922>"
-    if card == "10 c":
-        return "<:C10:716859635826556980>"
-    if card == "11 c":
-        return "<:CJ:716859641388335124>"
-    if card == "12 c":
-        return "<:CQ:716859653182718083>"
-    if card == "13 c":
-        return "<:CK:716859657972350977>"
-    return None
+    deck = []
+    for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]:
+        for value in range(1, 14):
+            card = Card(suit, value)
+            deck.append(card)
+    return deck
 
 def number_to_emoji(number):
     if number == 0:
@@ -224,7 +209,7 @@ def emoji_to_number(emoji):
     if emoji == "🔟":
         return 10
     return None
-   
+
 def string_to_emoji(word):
     emojis = ""
     letters = word.upper()
